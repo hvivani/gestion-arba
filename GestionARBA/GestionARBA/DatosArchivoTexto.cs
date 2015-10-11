55ee9ead1a125f0f40eaed482ef1ab4157033375
Hernan Vivani <hernan@vivani.com.ar>
2015-10-11 17:40:09 +0200

    Update DatosArchivoTexto.cs

--- GestionARBA/GestionARBA/DatosArchivoTexto.cs
+++ GestionARBA/GestionARBA/DatosArchivoTexto.cs
@@ -1,4 +1,12 @@
-﻿/*    This program is free software: you can redistribute it and/or modify
+/*  
+    DatosArchivoTexto.cs
+
+    Author:
+         Hernan Vivani <hernan@vivani.com.ar>
+
+    Copyright © SOffT 2010
+
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
